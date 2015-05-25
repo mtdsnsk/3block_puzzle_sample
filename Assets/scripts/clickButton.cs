7c5b5faeae3579deb00556007c43510aa1b08c9f
shinsuke matsuda <shinsuke@lotusandcompanyinc.com>
2015-05-26 01:07:56 +0900

    ref

---
+++ Assets/scripts/clickButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clickButton : MonoBehaviour {
+
+	public GameObject button;
+	public GameObject obj;
+
+	public void ClickTest () {
+
+		Debug.Log ("Clicked.");
+		Instantiate (obj, new Vector3 (0, 0, 0), Quaternion.identity);
+		Destroy(button);
+	}
+}
